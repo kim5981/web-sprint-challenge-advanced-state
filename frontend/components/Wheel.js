3da50837cc7d8f66f42e0875d97cb2dcc34cd299
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-04-22 16:44:57 -0700

    all active cogs responding to styling. one test failing, need to debug

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -22,27 +22,45 @@ export function Wheel(props) {
       {/* --i is a custom CSS property, no need to touch that nor the style object */}
       <div id="wheel">
 
-        <div className="cog active" style={{ "--i": 0 }}>
+        <div 
+          className={ `${props.wheel === 0 ? "cog active" : "cog" }` } 
+          style={{ "--i": 0 }}
+        >
           { props.wheel === 0 ? "B" : "" }
         </div>
 
-        <div className="cog" style={{ "--i": 1 }}>
+        <div 
+          className={ `${props.wheel === 1 ? "cog active" : "cog" }` } 
+          style={{ "--i": 1 }}
+        >
           { props.wheel === 1 ? "B" : "" }
         </div>
 
-        <div className="cog" style={{ "--i": 2 }}>
+        <div 
+          className={ `${props.wheel === 2 ? "cog active" : "cog" }` } 
+          style={{ "--i": 2 }}
+        >
           { props.wheel === 2 ? "B" : "" }
         </div>
         
-        <div className="cog" style={{ "--i": 3 }}>
+        <div
+          className={ `${props.wheel === 3 ? "cog active" : "cog" }` } 
+          style={{ "--i": 3 }}
+        >
           { props.wheel === 3 ? "B" : "" }
         </div>
         
-        <div className="cog" style={{ "--i": 4 }}>
+        <div 
+        className={ `${props.wheel === 4 ? "cog active" : "cog" }` } 
+        style={{ "--i": 4 }}
+        >
           { props.wheel === 4 ? "B" : "" }
         </div>
         
-        <div className="cog" style={{ "--i": 5 }}>
+        <div 
+          className={ `${props.wheel === 5 ? "cog active" : "cog" }` }
+          style={{ "--i": 5 }}
+        >
           { props.wheel === 5 ? "B" : "" }
         </div>
      
