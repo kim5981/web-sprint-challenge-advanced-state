e135783e09f19f1c2157256a910521c8ea89d11f
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-04-21 20:36:02 -0700

    change + submit handlers

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -4,21 +4,49 @@ import * as actionCreators from '../state/action-creators'
 
 export function Form(props) {
 
-  const onChange = evt => {
+  const { inputChange, form, postQuiz } = props
 
+  const onChange = evt => {
+    const { value, id } = evt.target
+    inputChange(value, id); 
   }
 
   const onSubmit = evt => {
+    evt.preventDefault()
+    postQuiz(form)
+  }
 
+  const disabledBtnHandler = () => {
+    return null
   }
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn">Submit new quiz</button>
+      <input 
+        maxLength={50} 
+        onChange={onChange}
+        id="newQuestion" 
+        placeholder="Enter question"
+      />
+      <input 
+        maxLength={50}
+        onChange={onChange} 
+        id="newTrueAnswer" 
+        placeholder="Enter true answer" 
+      />
+      <input
+        maxLength={50}
+        onChange={onChange}
+        id="newFalseAnswer"
+        placeholder="Enter false answer"
+      />
+      <button
+        id="submitNewQuizBtn"
+        disabled={ disabledBtnHandler() }
+      >
+      Submit new quiz
+      </button>
     </form>
   )
 }
